a5501b0d9e082528c2fefa900bf10ff0d1769501
Evangelos Spyromilios <71439594+V-Spyromilios@users.noreply.github.com>
2023-06-01 15:00:00 +0200

    Initial Commit

---
+++ Explore Mars/Explore_MarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Explore_MarsApp.swift
+//  Explore Mars
+//
+//  Created by Evangelos Spyromilios on 01.06.23.
+//
+
+import SwiftUI
+
+@main
+struct Explore_MarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
